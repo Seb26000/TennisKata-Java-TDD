34fe77002a49674e2138fdff85d532785340c864
Seb26000 <bergeses@PC-B124-08.iut-valence.fr>
2015-10-21 16:37:18 +0200

    Green.

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,9 +2,20 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 
+	private String score;
+	
+	public TennisGame(){
+		score = "Love-all";
+	}
+	
 	public String getScore() 
 	{
-		return "Love-all";
+		return score;
+	}
+
+	public void serverHasScored() 
+	{
+		score = "Fifteen-love";
 	}
 
 }
